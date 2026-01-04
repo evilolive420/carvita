98825a36b0015223d1e7174131b2941c59d52f6c
OpCode1300 <alex@opcode1300.com>
2026-01-03 21:08:46 -0500

    More updates

--- lib/presentation/screens/settings/settings_screen.dart
+++ lib/presentation/screens/settings/settings_screen.dart
@@ -966,7 +966,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                         _triggerNotificationReschedule();
                       }
                     },
-                    activeColor: Theme.of(context).colorScheme.primary,
+                    activeThumbColor: Theme.of(context).colorScheme.primary,
                   ),
                 ),
                 _buildSettingItem(
